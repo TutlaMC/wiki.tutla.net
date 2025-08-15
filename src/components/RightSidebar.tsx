55fb6e317d053114c8c2503eb6738b09a6c8085e
TutlaMC <ayaanibrahim1110@gmail.com>
2025-08-15 23:14:28 +0530

    fix

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,4 +1,4 @@
-export default function RightSidebar(data: JSON) {
+export default function RightSidebar({ data }: { data: any }) {
   return (
     <aside className="sticky top-0 max-h-screen w-60 p-4 border-l border-gray-700">
       <h2 className="font-bold mb-2">{String(data.title)}</h2>
