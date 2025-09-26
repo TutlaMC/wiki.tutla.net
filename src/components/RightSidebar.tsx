ce7130e67fe3b85242809a91930de537823e6712
TutlaMC <ayaanibrahim1110@gmail.com>
2025-09-27 00:53:57 +0530

    sidebar fix

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,15 +1,20 @@
+"use client";
+import { isMobile } from "react-device-detect";
+
 interface RightSidebarProps {
   data: {
-    title?: string,
-    summary?:string
-  }
+    title?: string;
+    summary?: string;
+  };
 }
 
 export default function RightSidebar({ data }: RightSidebarProps) {
+  if (isMobile) return null;
+
   return (
     <aside className="sticky top-0 max-h-screen w-60 p-4 border-l border-gray-700">
       <h2 className="font-bold mb-2">{data.title ?? ""}</h2>
       <p>{data.summary}</p>
     </aside>
-  )
+  );
 }
