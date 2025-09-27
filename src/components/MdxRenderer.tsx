8bcf4bdeab6c45d1319671bd8f4bbe32a9edeec9
TutlaMC <ayaanibrahim1110@gmail.com>
2025-09-27 14:16:08 +0530

    more fixes

--- src/components/MdxRenderer.tsx
+++ src/components/MdxRenderer.tsx
@@ -1,11 +1,16 @@
 "use client"
+import dynamic from 'next/dynamic'
+import { MDXRemoteSerializeResult } from "next-mdx-remote"
 
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote"
-import Notif from "./Notif"
+const MDXRemote = dynamic(() => import('next-mdx-remote').then(mod => ({ default: mod.MDXRemote })), {
+  ssr: false,
+  loading: () => <div>Loading content...</div>
+})
+
 interface MdxRendererProps {
   mdxSource: MDXRemoteSerializeResult
 }
 
 export default function MdxRenderer({ mdxSource }: MdxRendererProps) {
-  return <MDXRemote {...mdxSource} components={{Notif}} />
+  return <MDXRemote {...mdxSource} components={{ Notif: () => <div>Notif component</div> }} />
 }
